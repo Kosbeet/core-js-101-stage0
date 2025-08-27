233174cb7498d1081954fd64d9e4fd8f70f7f852
Konstantin <beeters88@gmail.com>
2025-08-27 23:33:34 +0300

    feat: add solutions

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -136,8 +136,16 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  if (
+    rect1.left > rect2.left + rect2.width ||
+    rect2.left > rect1.left + rect1.width ||
+    rect1.top > rect2.top + rect2.height ||
+    rect2.top > rect1.top + rect1.height
+  ) {
+    return false;
+  }
+  return true;
 }
 
 /**
@@ -166,8 +174,11 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const diffX = point.x - circle.center.x;
+  const diffY = point.y - circle.center.y;
+  const distance = Math.sqrt(diffX ** 2 + diffY ** 2);
+  return circle.radius > distance;
 }
 
 /**
@@ -181,8 +192,13 @@ function isInsideCircle(/* circle, point */) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str.indexOf(str[i]) === i && str.lastIndexOf(str[i]) === i) {
+      return str[i];
+    }
+  }
+  return null;
 }
 
 /**
@@ -207,8 +223,13 @@ function findFirstSingleChar(/* str */) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const borderStart = isStartIncluded ? '[' : '(';
+  const borderEnd = isEndIncluded ? ']' : ')';
+  if (a > b) {
+    return `${borderStart}${b}, ${a}${borderEnd}`;
+  }
+  return `${borderStart}${a}, ${b}${borderEnd}`;
 }
 
 /**
@@ -223,8 +244,12 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str
+    .split(' ')
+    .map((elem) => elem.split('').reverse().join(''))
+    .reverse()
+    .join(' ');
 }
 
 /**
@@ -239,8 +264,8 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return +num.toFixed().split('').reverse().join('');
 }
 
 /**
